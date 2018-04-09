6276eb0bf0108a825e9943ea707224f837fc8799
tronghungkaka <evocointeam@gmail.com>
2018-04-09 15:46:37 +0700

    changes crossorigin

--- src/main/java/com/evo/trade/HomeController.java
+++ src/main/java/com/evo/trade/HomeController.java
@@ -21,7 +21,7 @@ import com.evo.trade.objects.BollingerBand;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "https://evotradeapp.herokuapp.com", "https://dreamerstradeapp.herokuapp.com" })
+@CrossOrigin(origins = {"http://localhost:4200", "https://evocoinapp.herokuapp.com"})
 public class HomeController {
 
 	@RequestMapping("/")
